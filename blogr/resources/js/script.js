1fd158eab41a4f6ae46065bba03b23afc083b84c
Manuel Arellano <77225393+M-Arellano@users.noreply.github.com>
2021-10-11 10:16:11 -0400

    Add project files

---
+++ blogr/resources/js/script.js
@@ -0,0 +1,11 @@
+
+
+/*Use this to display the mobile nav*/ 
+function toggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
